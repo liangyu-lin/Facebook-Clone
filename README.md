0a676e3a7472da31f49b2e2ecd31393373e27700
LiangYu <liangyulin0727@gmail.com>
2021-02-17 00:38:11 -0800

    added instagram iframe

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 In this facebook clone, I learned to work with firebase database, firebase deployment, react context api, react hooks with functional components, and google authentication through firebase.
 
 This clone can live update post messages and pass in user data through google authentication.
-the post messages uses an animation library called flip-move.
+I also included iframe to show my instagram page.
--- src/Widgets.js
+++ src/Widgets.js
@@ -4,24 +4,16 @@ function Widgets() {
   const [posts, setPosts] = useState([]);
   return (
     <div className='widgets'>
-      <div
-        class='fb-page'
-        data-href='https://www.facebook.com/react'
-        data-tabs='timeline'
-        data-width='340px'
-        data-height='1500px'
-        data-small-header='false'
-        data-adapt-container-width='true'
-        data-hide-cover='false'
-        data-show-facepile='true'
-      >
-        <blockquote
-          cite='https://www.facebook.com/react'
-          class='fb-xfbml-parse-ignore'
-        >
-          <a href='https://www.facebook.com/react'>React</a>
-        </blockquote>
-      </div>
+      <iframe
+        width='340'
+        height='100%'
+        style={{ border: "none", overflow: "hidden" }}
+        src='http://instagram.com/p/CHdsykXANRN/embed'
+        scrolling='no'
+        frameborder='0'
+        allowTransparency='true'
+        allow='encrypted-media'
+      ></iframe>
     </div>
   );
 }
